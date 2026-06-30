0efca2a54fc5887b60d13c6697ff15bf2c33de60
Artem057 <justarocketgame@gmail.com>
2026-06-30 14:54:48 +0300

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JustARocketGame/ArteHack/refs/heads/main/bl/v7.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JustARocketGame/ArteHack/refs/heads/main/bl/v8.lua"))()
